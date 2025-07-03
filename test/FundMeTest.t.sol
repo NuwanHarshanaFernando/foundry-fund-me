b2b39ad5f1ba3b0fa3b3d48316bb4c1fe2c1eb83
root <root@DESKTOP-932DINA.localdomain>
2025-07-03 16:09:30 +0530

    Testing 2

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -3,17 +3,25 @@
 pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    uint256 number = 1;
+    FundMe fundMe;
 
     function setUp() external {
-        number = 2;
+        // FundMe fundMe = new FundMe(); // fundMe variable of type FundMe is a new FundMe contract
+        // us -> FundMeTest -> FundMe -> The reason for the error of testOwnerIsMsgSender
+        fundMe = new FundMe();
     }
 
-    function testDemo() public {
-        console.log(number);
-        console.log("hi");
-        assertEq(number, 2);
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        console.log(fundMe.i_owner());
+        console.log(msg.sender);
+        //  assertEq(fundMe.i_owner(), msg.sender); // Fails because of (us -> FundMeTest -> FundMe)
+        assertEq(fundMe.i_owner(), address(this)); // Now test pass
     }
 }
